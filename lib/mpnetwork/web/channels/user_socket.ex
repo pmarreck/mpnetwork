e53c320e3199775a612bff9b0373286479d13980
Peter Marreck <lumbergh@gmail.com>
2017-04-22 11:56:05 -0400

    More changes related to Phoenix 1.3-rc upgrade and troubleshooting a deployment issue

--- lib/mpnetwork/web/channels/user_socket.ex
+++ lib/mpnetwork/web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Mpnetwork.Web.UserSocket do
   # channel "room:*", Mpnetwork.Web.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
--- mix.exs
+++ mix.exs
@@ -36,7 +36,8 @@ defmodule Mpnetwork.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:guardian, "~> 0.14"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
