e784bc0e616b8e4f84514574a28f9176ab6132ac
Peter Marreck <lumbergh@gmail.com>
2017-05-18 16:18:14 -0400

    Modified configs to try deploying to gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -18,10 +18,10 @@ config :mpnetwork, Mpnetwork.Web.Endpoint,
   http: [port: {:system, "PORT"}],
   root: ".",
   server: true,
-  url: [scheme: "https", host: "murmuring-meadow-65796.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  # url: [scheme: "https", host: "murmuring-meadow-65796.herokuapp.com", port: 443],
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: "${SECRET_KEY_BASE}" #System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -74,7 +74,7 @@ config :logger, level: :info
 # Configure your database
 config :mpnetwork, Mpnetwork.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
+  url: {:system, "DATABASE_URL"}, #System.get_env("DATABASE_URL"),
   pool_size: 20,
   ssl: true
 
