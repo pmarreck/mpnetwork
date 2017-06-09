42eff443f2424c9773fb5fa3f4356078cad483ab
Peter Marreck <lumbergh@gmail.com>
2017-06-08 20:50:59 -0400

    First attempt to serve static assets without cookies from the 'static' subdomain

--- lib/mpnetwork/web/endpoint.ex
+++ lib/mpnetwork/web/endpoint.ex
@@ -8,8 +8,9 @@ defmodule Mpnetwork.Web.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :mpnetwork, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    at: "/", from: :mpnetwork, gzip: true,
+    only: ~w(css fonts images js favicon.ico robots.txt),
+    headers: [{"access-control-allow-origin", "*"}]
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
