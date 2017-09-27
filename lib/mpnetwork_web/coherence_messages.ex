9f070d9a1526a9944c8d1d5fb280207bfd906f3f
Peter Marreck <lumbergh@gmail.com>
2017-09-27 15:28:44 -0400

    Upped versions of dependencies.

--- lib/mpnetwork_web/coherence_messages.ex
+++ lib/mpnetwork_web/coherence_messages.ex
@@ -75,4 +75,5 @@ defmodule MpnetworkWeb.Coherence.Messages do
     do: dgettext(@domain, "You are using an invalid security token for this site! This security\n" <>
       "violation has been logged.\n")
   def mailer_required, do: dgettext(@domain, "Mailer configuration required!")
+  def account_is_inactive, do: dgettext(@domain, "Your account is inactive.")
 end
