2dc6c6d2f3f3b6377efc8e874d7cf8c4031baec8
Peter Marreck <lumbergh@gmail.com>
2017-10-10 13:31:35 -0400

    Relaxed validations on non-draft listings, fixed bug with URL's not showing on listing/show

--- lib/mpnetwork/realtor/listing.ex
+++ lib/mpnetwork/realtor/listing.ex
@@ -218,7 +218,7 @@ defmodule Mpnetwork.Realtor.Listing do
   def changeset(%Listing{} = listing, attrs) do
     listing
     |> casts(attrs)
-    |> validate_required([:user_id, :broker_id, :draft, :for_sale, :for_rent, :address, :city, :state, :zip, :price_usd, :num_bedrooms, :num_baths, :num_half_baths, :sq_ft, :lot_size, :year_built, :stories, :basement, :num_fireplaces, :parking_spaces, :num_garages, :attached_garage, :new_construction, :prop_tax_usd, :central_air, :visible_on, :expires_on])
+    |> validate_required([:user_id, :broker_id, :draft, :for_sale, :for_rent, :address, :city, :state, :zip, :price_usd, :num_bedrooms, :num_baths, :num_half_baths, :sq_ft, :year_built, :basement, :visible_on, :expires_on])
     |> constraints
   end
 
