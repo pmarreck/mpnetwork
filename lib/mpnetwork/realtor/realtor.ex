e8f3c09d7ea2169f59b115e86404b184e0db479e
Peter Marreck <lumbergh@gmail.com>
2017-11-03 13:51:18 -0400

    Small change to make inspection start times less than 4 hours ago still show up on inspection sheets

--- lib/mpnetwork/realtor/realtor.ex
+++ lib/mpnetwork/realtor/realtor.ex
@@ -233,10 +233,12 @@ defmodule Mpnetwork.Realtor do
   def list_next_broker_oh_listings(_, _, _after_datetime \\ nil)
   def list_next_broker_oh_listings(nil, number, after_datetime) do
     now = after_datetime || NaiveDateTime.utc_now
+    now = now |> Timex.shift(hours: -4) |> Timex.to_naive_datetime # so 9am inspections still show up on sheet at 1pm (but not later)
     Repo.all(from l in Listing, where: l.next_broker_oh_start_at > ^now and l.draft == false, order_by: [asc: l.next_broker_oh_start_at], limit: ^number, preload: [:broker, :user])
   end
   def list_next_broker_oh_listings(current_user, number, after_datetime) do
     now = after_datetime || NaiveDateTime.utc_now
+    now = now |> Timex.shift(hours: -4) |> Timex.to_naive_datetime # so 9am inspections still show up on sheet at 1pm (but not later)
     Repo.all(from l in Listing, where: l.next_broker_oh_start_at > ^now and l.draft == false or l.user_id == ^current_user.id, order_by: [asc: l.next_broker_oh_start_at], limit: ^number, preload: [:broker, :user])
   end
 
