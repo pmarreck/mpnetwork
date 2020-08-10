eb2227dfe47cfb350bc520a08f1b3b2b8adce111
Peter Marreck <lumbergh@gmail.com>
2020-08-10 16:22:54 -0400

    Standard broker template borked in email client notifications so trying a classy email template I found online instead

--- lib/mpnetwork_web/views/email_view.ex
+++ lib/mpnetwork_web/views/email_view.ex
@@ -1,3 +1,6 @@
 defmodule Mpnetwork.EmailView do
   use MpnetworkWeb, :view
+  def nextphoto(nil), do: {nil, nil}
+  def nextphoto([]), do: {nil, nil}
+  def nextphoto([head | rest]), do: {head, rest}
 end
