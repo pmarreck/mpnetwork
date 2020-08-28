59b0ddbc6e6c1e315570a515bc35cf3daf2ff0ec
Peter Marreck <peter@marreck.com>
2020-08-28 20:51:46 +0000

    Added Logflare logging WITH NEWLY-REGENERATED ACTUALLY-SECRET API KEY FOR PUBLIC REPO THIS TIME 😂

--- config/prod.exs
+++ config/prod.exs
@@ -39,7 +39,7 @@ config :logger, level: :info, backends: [LogflareLogger.HttpBackend]
 config :logflare_logger_backend,
   url: "https://api.logflare.app", # https://api.logflare.app is configured by default and you can set your own url
   level: :info, # Default LogflareLogger level is :info. Note that log messages are filtered by the :logger application first
-  api_key: "bTydtgMfXAcj",
+  api_key: System.fetch_env!("LOGFLARE_API_KEY"),
   source_id: "a59afa68-67a7-42a5-8738-b081bc00f85b",
   flush_interval: 1_000, # minimum time in ms before a log batch is sent to the server ",
   max_batch_size: 50 # maximum number of events before a log batch is sent to the server
