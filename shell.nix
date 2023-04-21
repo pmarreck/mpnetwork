1e7bca1ec4222647446c8d54e9af93870023cbb0
Peter Marreck <lumbergh@gmail.com>
2023-04-21 15:25:02 -0400

    Updated flake-utils, nixpkgs, and systems dependencies, and bumped elixir_make to version 0.7.6. Also added nix-direnv to the shell.nix file.

--- shell.nix
+++ shell.nix
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-# run this with: nix-shell --pure --show-trace
-# let
-#   unstable = import (fetchTarball {
-#     url = https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz;
-#     sha256 = "150xy795rzzy09xidkyrysga6khv5d4ikfz98s2n77a38d7njdwj";
-#   }) {
-#     inherit system;
-#   };
-# in
-||||||| parent of 589b887 (Update dependencies in flake.lock and shell.nix, including erlang, elixir, nodejs, and postgresql. Also add gigalixir to shell.nix and remove mix2nix due to compatibility issues with erlang 25. Update system definition in shell.nix to support darwin on x86_64 architecture.)
-# run this with: nix-shell --pure --show-trace
-let
-  unstable = import <nixos-unstable> { }; #(fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
-in
-=======
 ## run this with: nix-shell --pure --show-trace
 ## or via flakes: nix develop
 ## note: on darwin there's a few things to know.
@@ -24,66 +8,26 @@ in
 ## NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1 nix develop --system x86_64-darwin --impure
 ## or like so for the traditional Nix (sans Flakes) approach:
 ## NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1 nix-shell --system x86_64-darwin --impure
-let
-  unstable = import (
-    builtins.fetchTarball {
-      url = "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz";
-      sha256 = "0jw0wyv3pvzi0pd80zv6y00rvmvisjlns0n70vl0a7m3l9qz24ss"; 
-    });
-in
->>>>>>> 589b887 (Update dependencies in flake.lock and shell.nix, including erlang, elixir, nodejs, and postgresql. Also add gigalixir to shell.nix and remove mix2nix due to compatibility issues with erlang 25. Update system definition in shell.nix to support darwin on x86_64 architecture.)
 # { pkgs ? import <nixpkgs> { overlays = [(import ./glibc-overlay.nix)]; } }:
-<<<<<<< HEAD
 { system ? builtins.currentSystem, pkgs ? import <nixpkgs> { inherit system; } }:
-||||||| parent of 589b887 (Update dependencies in flake.lock and shell.nix, including erlang, elixir, nodejs, and postgresql. Also add gigalixir to shell.nix and remove mix2nix due to compatibility issues with erlang 25. Update system definition in shell.nix to support darwin on x86_64 architecture.)
-{ pkgs ? import <nixpkgs> { } }:
-=======
-{ 
-  pkgs ? unstable { inherit system; },
-  system ? (if pkgs.stdenv.isDarwin then "darwin" else "linux"),
-  # pkgs_aarch64 ? import <nixpkgs> { localSystem = "aarch64-darwin"; },
-  # pkgs_x86_64 ? import <nixpkgs> { localSystem = "x86_64-darwin"; },
-}:
->>>>>>> 589b887 (Update dependencies in flake.lock and shell.nix, including erlang, elixir, nodejs, and postgresql. Also add gigalixir to shell.nix and remove mix2nix due to compatibility issues with erlang 25. Update system definition in shell.nix to support darwin on x86_64 architecture.)
 with pkgs;
 let
   inherit (lib) optional optionals;
   inherit (stdenv) isLinux isDarwin;
-<<<<<<< HEAD
   unstable = import (fetchTarball {
     url = https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz;
-    sha256 = "0vczd1pgsgnknn814y6bx9vdzhrs78x0m80rzihjv19wz596sril";
+    sha256 = "0kga5cljz6p67irlw2al1shwr5slv0x31fm9cj9xvimghhf1d022";
   }) {
     inherit system;
   };
   erlang = unstable.erlang; # I like to live dangerously. For fallback, use stable of: # erlangR25;
-||||||| parent of 589b887 (Update dependencies in flake.lock and shell.nix, including erlang, elixir, nodejs, and postgresql. Also add gigalixir to shell.nix and remove mix2nix due to compatibility issues with erlang 25. Update system definition in shell.nix to support darwin on x86_64 architecture.)
-  # like a .tool-versions for Nix...
-  erlang = erlangR25;
-=======
-  erlang = erlangR25;
->>>>>>> 589b887 (Update dependencies in flake.lock and shell.nix, including erlang, elixir, nodejs, and postgresql. Also add gigalixir to shell.nix and remove mix2nix due to compatibility issues with erlang 25. Update system definition in shell.nix to support darwin on x86_64 architecture.)
   elixir = beam.packages.erlangR25.elixir_1_14;
   nodejs = nodejs-16_x;
   postgresql = postgresql_13;
-<<<<<<< HEAD
   # inherit (callPackage (fetchGit {
   #   url = https://gitlab.com/transumption/mix-to-nix;
   #   rev = "b70cb8f7fca80d0c5f7539dbfec497535e07d75c";
   # }) {}) mixToNix;
-||||||| parent of 589b887 (Update dependencies in flake.lock and shell.nix, including erlang, elixir, nodejs, and postgresql. Also add gigalixir to shell.nix and remove mix2nix due to compatibility issues with erlang 25. Update system definition in shell.nix to support darwin on x86_64 architecture.)
-    inherit (callPackage (fetchGit {
-    url = https://gitlab.com/transumption/mix-to-nix;
-    rev = "b70cb8f7fca80d0c5f7539dbfec497535e07d75c";
-  }) {}) mixToNix;
-=======
-  ### as of 3/2023, mix2nix breaks on erlang 25 and hasn't been updated in 4 years
-  ### I wasn't really using it yet, anyway, but it's a nice idea
-  # inherit (callPackage (fetchGit {
-  #   url = https://gitlab.com/transumption/mix-to-nix;
-  #   rev = "b70cb8f7fca80d0c5f7539dbfec497535e07d75c";
-  # }) {}) mixToNix;
->>>>>>> 589b887 (Update dependencies in flake.lock and shell.nix, including erlang, elixir, nodejs, and postgresql. Also add gigalixir to shell.nix and remove mix2nix due to compatibility issues with erlang 25. Update system definition in shell.nix to support darwin on x86_64 architecture.)
 in
 mkShell {
 
@@ -107,6 +51,7 @@ mkShell {
     wget
     libiconv
     direnv
+    nix-direnv
     # $%&* locales...
     glibcLocales
     glibc
@@ -117,24 +62,11 @@ mkShell {
     erlang
     elixir
     postgresql
-<<<<<<< HEAD
-||||||| parent of 589b887 (Update dependencies in flake.lock and shell.nix, including erlang, elixir, nodejs, and postgresql. Also add gigalixir to shell.nix and remove mix2nix due to compatibility issues with erlang 25. Update system definition in shell.nix to support darwin on x86_64 architecture.)
-    gigalixir
-    mix2nix
-=======
     # mix2nix
->>>>>>> 589b887 (Update dependencies in flake.lock and shell.nix, including erlang, elixir, nodejs, and postgresql. Also add gigalixir to shell.nix and remove mix2nix due to compatibility issues with erlang 25. Update system definition in shell.nix to support darwin on x86_64 architecture.)
     which
     ripgrep
-<<<<<<< HEAD
-  ] ++ optional isLinux gigalixir # it is currently broken on darwin as of 3/10/2023 =(
-    ++ optional isLinux inotify-tools
-||||||| parent of 589b887 (Update dependencies in flake.lock and shell.nix, including erlang, elixir, nodejs, and postgresql. Also add gigalixir to shell.nix and remove mix2nix due to compatibility issues with erlang 25. Update system definition in shell.nix to support darwin on x86_64 architecture.)
-  ] ++ optional isLinux inotify-tools
-=======
   ] ++ optional isLinux gigalixir # gigalixir CLI is broken on darwin as of 3/2023; need to install some other way impurely!
     ++ optional isLinux inotify-tools
->>>>>>> 589b887 (Update dependencies in flake.lock and shell.nix, including erlang, elixir, nodejs, and postgresql. Also add gigalixir to shell.nix and remove mix2nix due to compatibility issues with erlang 25. Update system definition in shell.nix to support darwin on x86_64 architecture.)
     ++ optional isLinux libnotify
     ++ optional isDarwin terminal-notifier
     ++ optionals isDarwin (with darwin.apple_sdk.frameworks; [
